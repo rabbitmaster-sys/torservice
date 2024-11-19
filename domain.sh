a0ffe2591780ffb9e7e94b5332da6b63769da836
rabbitmaster-sys <rigerphin@gmail.com>
2024-11-19 18:05:05 +0530

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -1,10 +1,10 @@
-function TOR_DOMAIN() {
-  if [ -f ./tor/hidden_service/hostname ]; then
-    domain=$(cat ./tor/hidden_service/hostname)
-    echo -e "\033[32mhttp://$domain/\033[0m"
-    echo "http://$domain/" > ./public/domains.html
-  else
-    echo "Hidden service hostname not found. Tor may not be fully initialized yet." > ./public/domains.html
-  fi
+function TOR_DOMAIN(){
+  while [ ! -f ./tor/hidden_service/hostname ]; do
+    echo "Waiting for Tor hidden service to initialize..."
+    sleep 2
+  done
+  domain=$(cat ./tor/hidden_service/hostname)
+  echo -e "\033[32mhttp://$domain/\033[0m"
+  echo "http://$domain/" > ./public/domains.html
 }
 TOR_DOMAIN
